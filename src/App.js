fc54a072da605bcf9789356d144fa77c2e387885
Mimii15 <miminutmeg@gmail.com>
2022-12-29 14:33:32 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Weather App</h1>
-			<footer>
-				This project was coded by{" "}
-				<a href="https://cristiana-santos.netlify.app/" target="_blank">
-					Cristiana Santos
-				</a>{" "}
-				and is{" "}
-				<a href="https://github.com/Mimii15/react-app" target="_blank">
-					open-sourced on Github
-				</a>
-			</footer>
+			<div className="container">
+				<h1>Weather App</h1>
+
+				<footer>
+					This project was coded by{" "}
+					<a
+						href="https://cristiana-santos.netlify.app/"
+						target="_blank"
+					>
+						Cristiana Santos
+					</a>{" "}
+					and is{" "}
+					<a
+						href="https://github.com/Mimii15/react-app"
+						target="_blank"
+					>
+						open-sourced on Github
+					</a>
+				</footer>
+			</div>
 		</div>
 	);
 }
