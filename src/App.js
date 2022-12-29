b41457438de5d433515079db6efa53f9b5407a10
Mimii15 <miminutmeg@gmail.com>
2022-12-29 14:02:03 +0000

    added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-	return <div className="App">Weather App</div>;
+	return (
+		<div className="App">
+			<h1>Weather App</h1>
+			<footer>
+				This project was coded by{" "}
+				<a href="https://cristiana-santos.netlify.app/" target="_blank">
+					Cristiana Santos
+				</a>{" "}
+				and is{" "}
+				<a href="https://github.com/Mimii15/react-app" target="_blank">
+					open-sourced on Github
+				</a>
+			</footer>
+		</div>
+	);
 }
